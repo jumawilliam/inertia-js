ef41ba020a664b403c85ca575fbff5dc059ca4b4
william <jumawilliam2011@gmail.com>
2023-04-02 19:48:57 +0300

    inertia project

---
+++ resources/js/Pages/create.vue
@@ -0,0 +1,31 @@
+<script setup>
+import Layout from './Layout'
+
+
+</script>
+
+<template>
+    <div class="container">
+        <br>
+      <div class="card col-6 offset-3">
+        <h5 class="card-header">Create User</h5>
+        <div class="card-body">
+            <form>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Name</label>
+                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Email</label>
+                <input type="email" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Phone</label>
+                <input type="number" class="form-control" id="exampleInputPassword1">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+        </div>
+    </div>
+</template>
